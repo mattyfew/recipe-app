64aa497eaecc186d21081796df6765b5ac38e062
Matt Fewer <mattfewer@hotmail.com>
2017-05-11 22:51:18 +0200

    added several functionalities

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient){
+      console.log("something")
+      this.ingredients.push(ingredient)
+  }
+
 }
